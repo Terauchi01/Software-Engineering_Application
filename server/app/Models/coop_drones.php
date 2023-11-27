f1e0901f907e0497ab61469f0af0c9ef1eca38fa
“Terauchi01” <shun1572jp@gmail.com>
2023-11-27 14:42:06 +0900

    データベース関係の空ファイル追加

---
+++ server/app/Models/coop_drones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class coop_drones extends Model
+{
+    use HasFactory;
+}
---
+++ server/database/factories/delivery_location_imageFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\delivery_location_image>
+ */
+class delivery_location_imageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
