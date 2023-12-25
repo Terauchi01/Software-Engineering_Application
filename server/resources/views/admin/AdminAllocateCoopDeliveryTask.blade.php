d6277d37a685ea0557e46d855b135fbdf63053c7
“Terauchi01” <shun1572jp@gmail.com>
2023-12-25 12:12:43 +0900

    課題見た目の挿入,線を直書きしてる

--- server/resources/views/admin/AdminAllocateCoopDeliveryTask.blade.php
+++ server/resources/views/admin/AdminAllocateCoopDeliveryTask.blade.php
@@ -1,1 +1,139 @@
 adminAllocateCoopDeliveryTask
+<div style="width: 100%; height: 100%; position: relative; background: #FAFAFA">
+    <div style="width: 24px; height: 24px; left: 1265px; top: 508px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 598px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 688px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 778px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 868px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 958px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 494px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 584px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 674px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 764px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 854px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1190px; height: 0px; left: 193px; top: 944px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 1040.11px; height: 0px; left: 191.94px; top: 1124.57px; position: absolute; border: 0.20px #D3D3D3 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 417px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 507px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 597px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 687px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 777px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 867px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 957px; position: absolute; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px; border: 1px #A1A1A1 solid"></div>
+    <div style="width: 24px; height: 24px; left: 1265px; top: 418px; position: absolute">
+        <div style="width: 16px; height: 18px; left: 4px; top: 3px; position: absolute; background: #B3B3B3"></div>
+    </div>
+    <div style="width: 240px; height: 36px; left: 1072px; top: 280px; position: absolute">
+        <div style="width: 79px; height: 36px; left: 161px; top: 0px; position: absolute; background: white; box-shadow: 0px 3px 50px rgba(0, 0, 0, 0.08); border-radius: 4px; border: 1px #408A7E solid"></div>
+        <div style="width: 47px; left: 177px; top: 9px; position: absolute; text-align: center; color: #408A7E; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">削除</div>
+        <div style="width: 157px; left: 0px; top: 9px; position: absolute; text-align: center; color: #408A7E; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">チェックを入れた要素を</div>
+    </div>
+    <div style="width: 180px; height: 652px; left: 507px; top: 348px; position: absolute">
+        <div style="left: 0px; top: 72px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名</span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">1<br/>住所 : わかめ県こんぶ町</span></div>
+        <div style="left: 0px; top: 160px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名2<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県めかぶ町</span></div>
+        <div style="left: 1px; top: 253px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名3<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県かつお町</span></div>
+        <div style="left: 0px; top: 343px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名4<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県マシュマロ町</span></div>
+        <div style="left: 1px; top: 433px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名5<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県こんぶ町</span></div>
+        <div style="left: 1px; top: 526px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名6<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県こんぶ町</span></div>
+        <div style="left: 1px; top: 614px; position: absolute"><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">送り主名7<br/></span><span style="color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">住所 : わかめ県こんぶ町</span></div>
+        <div style="left: 5px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">送り主情報</div>
+    </div>
+    <div style="width: 104px; height: 18px; left: 335px; top: 286px; position: absolute; color: #9D9D9D; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">22 items</div>
+    <div style="width: 842px; height: 19px; left: 290px; top: 348px; position: absolute">
+        <div style="width: 180px; height: 652px; left: 438px; top: 0px; position: absolute">
+            <div style="left: 0px; top: 72px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名2<br/>住所 : わかめ県めかぶ町</div>
+            <div style="left: 0px; top: 160px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名3<br/>住所 : わかめ県マシュマロ町</div>
+            <div style="left: 1px; top: 253px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名1<br/>住所 : わかめ県かつお町</div>
+            <div style="left: 0px; top: 343px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名4<br/>住所 : わかめ県こんぶ町</div>
+            <div style="left: 1px; top: 433px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名2<br/>住所 : わかめ県かつお町</div>
+            <div style="left: 1px; top: 526px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名1<br/>住所 : わかめ県めかぶ町</div>
+            <div style="left: 1px; top: 614px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">受取り主名1<br/>住所 : わかめ県マシュマロ町</div>
+            <div style="left: 5px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">受取り主情報</div>
+        </div>
+        <div style="width: 87px; height: 631px; left: -3px; top: 0px; position: absolute">
+            <div style="width: 56px; height: 17px; left: 0px; top: 74px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">1</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 164px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">2</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 254px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">3</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 344px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">4</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 434px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">5</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 524px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">6</div>
+            <div style="width: 56px; height: 17px; left: 0px; top: 614px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">7</div>
+            <div style="width: 84px; left: 3px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">依頼番号</div>
+        </div>
+        <div style="width: 93.02px; height: 0px; left: -105.01px; top: -170px; position: absolute; border: 3px #408A7E solid"></div>
+        <div style="width: 84px; height: 632px; left: 635px; top: 0px; position: absolute">
+            <div style="left: 14px; top: 73px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 163px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 253px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 343px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 433px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 523px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 14px; top: 613px; position: absolute; color: #525252; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者名</div>
+            <div style="left: 0px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">担当事業者名</div>
+        </div>
+        <div style="width: 28px; height: 721.59px; left: 859px; top: 0px; position: absolute">
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 73px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 163px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 253px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 343px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 433px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 523px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 613px; position: absolute; background: #B3B3B3"></div>
+            <div style="width: 18.59px; height: 18.59px; left: 5px; top: 703px; position: absolute; background: #B3B3B3"></div>
+            <div style="left: 0px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">編集</div>
+        </div>
+        <div style="left: 970px; top: 0px; position: absolute; color: #5F5F5F; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">削除</div>
+    </div>
+    <div style="width: 24px; height: 24px; left: 216px; top: 283px; position: absolute; background: white; box-shadow: 0px 2px 50px rgba(0, 0, 0, 0.10); border-radius: 4px"></div>
+    <div style="width: 72px; height: 22px; left: 251px; top: 286px; position: absolute; color: #747474; font-size: 14px; font-family: Noto Sans; font-weight: 700; word-wrap: break-word">Select all</div>
+    <div style="width: 99px; height: 36px; left: 434px; top: 278px; position: absolute">
+        <div style="width: 99px; height: 36px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 3px 50px rgba(0, 0, 0, 0.08); border-radius: 4px; border: 1px #408A7E solid"></div>
+        <div style="width: 46px; left: 22px; top: 6px; position: absolute; color: #408A7E; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">承認</div>
+    </div>
+    <div style="width: 79px; height: 36px; left: 580px; top: 278px; position: absolute">
+        <div style="width: 79px; height: 36px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 3px 50px rgba(0, 0, 0, 0.08); border-radius: 4px; border: 1px #408A7E solid"></div>
+        <div style="width: 47px; left: 18px; top: 6px; position: absolute; color: #408A7E; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">却下</div>
+    </div>
+    <div style="left: 862px; top: 228px; position: absolute; color: white; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">検索</div>
+    <div style="width: 127px; height: 1024px; left: 0px; top: 0px; position: absolute">
+        <div style="width: 127px; height: 1024px; left: 0px; top: 0px; position: absolute; background: #408A7E"></div>
+        <div style="width: 127px; height: 290px; left: 0px; top: 64px; position: absolute">
+            <div style="width: 127px; height: 38px; left: 0px; top: 252px; position: absolute; background: white"></div>
+            <div style="left: 15px; top: 260px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">宅配依頼一覧</div>
+            <div style="left: 7px; top: 214px; position: absolute; text-align: center; color: white; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">利用者情報分析</div>
+            <div style="left: 7px; top: 168px; position: absolute; text-align: center; color: white; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者情報分析</div>
+            <div style="left: 7px; top: 122px; position: absolute; text-align: center; color: white; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">利用者情報管理</div>
+            <div style="left: 15px; top: 54px; position: absolute; text-align: center; color: white; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">ドローン<br/>貸与申請一覧</div>
+            <div style="left: 7px; top: 0px; position: absolute; color: white; font-size: 16px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">事業者情報管理</div>
+        </div>
+    </div>
+    <div style="width: 1313px; height: 100px; left: 127px; top: 0px; position: absolute">
+        <div style="width: 1313px; height: 100px; left: 0px; top: 0px; position: absolute; background: white"></div>
+        <div style="left: 1037px; top: 29px; position: absolute; color: black; font-size: 20px; font-family: Noto Sans; font-weight: 600; word-wrap: break-word">管理者</div>
+        <div style="width: 107px; height: 17px; left: 1036px; top: 60px; position: absolute; color: black; font-size: 14px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">+918912345679</div>
+        <div style="width: 32px; height: 32px; left: 942px; top: 39px; position: absolute">
+            <div style="width: 26px; height: 29px; left: 3px; top: 1px; position: absolute; background: #408A7E"></div>
+            <div style="width: 8px; height: 8px; left: 22px; top: 4px; position: absolute; background: #408A7E"></div>
+        </div>
+        <div style="width: 70px; height: 70px; left: 1153px; top: 15px; position: absolute">
+            <div style="width: 70px; height: 70px; left: 0px; top: 0px; position: absolute; background: #EFEFEF; border-radius: 9999px"></div>
+            <div style="left: 23px; top: 10px; position: absolute; color: #868686; font-size: 36px; font-family: Noto Sans; font-weight: 400; word-wrap: break-word">A</div>
+        </div>
+    </div>
+    <div style="height: 26px; left: 185px; top: 152px; position: absolute">
+        <div style="width: 96px; height: 0px; left: 0px; top: 26px; position: absolute; border: 3px #408A7E solid"></div>
+        <div style="left: 0px; top: 0px; position: absolute; color: #408A7E; font-size: 16px; font-family: Noto Sans; font-weight: 600; word-wrap: break-word">宅配依頼一覧</div>
+    </div>
+</div>
