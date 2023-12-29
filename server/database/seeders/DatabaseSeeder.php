64433c90e1a3c32287e5f48f3ff7c1558a3f2338
“Terauchi01” <shun1572jp@gmail.com>
2023-12-29 16:06:24 +0900

    都道府県のテーブル追加

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    private const SEEDERS = [
+        MstPrefectureSeeder::class,
+    ];
     public function run(): void
     {
         AdminUser::factory(1)->create();
@@ -35,5 +38,8 @@ class DatabaseSeeder extends Seeder
         DeliveryRequest::factory(100)->create();
         Favorite::factory(100)->create();
         LicenseInformation::factory(100)->create();
+        foreach(self::SEEDERS as $seeder) {
+            $this->call($seeder);
+        }
     }
 }
