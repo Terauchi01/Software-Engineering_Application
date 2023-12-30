ab36267c5509327305fb396dc42b69dfa9a94b48
1250333semi <250333k@ugs.kochi-tech.ac.jp>
2023-12-30 17:04:00 +0900

    管理者の事業者一覧画面追加

--- server/app/Http/Controllers/AdminViewCoopListController.php
+++ server/app/Http/Controllers/AdminViewCoopListController.php
@@ -8,6 +8,16 @@ class AdminViewCoopListController extends Controller
 {
     public function adminViewCoopList (){
         //viewの返すところは適当で良い
-        return view('admin.AdminViewCoopList');
+        $id = ['1', '2', '3'];
+        $coop_name = ['土佐山田配送', '高知配送', '南国配送'];
+        $representative_name = ['山田太郎', '高知花子', '南国次郎'];
+        $coop_locations_list_id = ['土佐山田', '高知', '南国'];
+        $check = ['済', '済', '未'];
+        return view('admin.AdminViewCoopList', compact('id', 'coop_name', 'representative_name', 'coop_locations_list_id', 'check'));
     }
 }
+// id
+// coop_name
+// representative_name
+// coop_locations_list_id
+// land_or_air
