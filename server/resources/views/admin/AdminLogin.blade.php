00ec1cb65298a8a9eefddc0bc88e7c5edfb41bfd
S_Matsumoto <250371g@ugs.kochi-tech.ac.jp>
2023-12-31 18:20:33 +0900

    create login front for each user

--- server/resources/views/admin/AdminLogin.blade.php
+++ server/resources/views/admin/AdminLogin.blade.php
@@ -9,9 +9,14 @@
                 AeroNet
             </div>
 
+            @if($errors->has('login'))
+            <div>
+                {{ $errors->first('login') }}
+            </div>
+            @endif
             <form action="{{ route('admin.adminLoginFunction') }}" method="POST" class="login_form">
                 @csrf
-                <input type="text" name="email" placeholder="メールアドレス" required>
+                <input type="text" name="user_name" placeholder="ユーザ名" required>
                 <input type="password" name="password" placeholder="パスワード" required>
                 <button type="submit" class="login_">ログイン</button>
             </form>
