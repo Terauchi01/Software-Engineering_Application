0074c5f7890d2b1fb55abe528ef25aa1fa2c0a73
S_Matsumoto <250371g@ugs.kochi-tech.ac.jp>
2024-01-01 12:44:44 +0900

    create logout blade, css

--- server/resources/views/admin/AdminLogout.blade.php
+++ server/resources/views/admin/AdminLogout.blade.php
@@ -1,1 +1,16 @@
-adminLogout
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <link rel="stylesheet" href="{{ asset('/css/admin/AdminLogout.css') }}">
+    </head>
+    <body>
+        <div class="logout">
+            <form action="{{ route('admin.adminLogout') }}" method="POST" class="logout_form">
+                @csrf
+                <p>ログアウトしますか</p>
+                <button type="submit" name="logout" value="true">ログアウト</button>
+                <button type="submit" name="logout" value="false">トップへ戻る</button>
+            </form>
+        </div>
+    </body>
+</html>
