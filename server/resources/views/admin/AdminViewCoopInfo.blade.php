572f5c8f9f095340a828e47f43e0664d163f98ae
S_Matsumoto <250371g@ugs.kochi-tech.ac.jp>
2024-01-12 12:59:20 +0900

    create view coop info, add sidebar

--- server/resources/views/admin/AdminViewCoopInfo.blade.php
+++ server/resources/views/admin/AdminViewCoopInfo.blade.php
@@ -1,1 +1,58 @@
-adminViewCoopInfo
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <link rel="stylesheet" href="{{ asset('/css/admin/AdminViewCoopInfo.css') }}">
+    </head>
+    <body>
+        <nav class="side">
+            <div class="current">
+                <p><a href="{{ route('admin.adminViewCoopList') }}">事業者情報管理</a></p>
+            </div>
+            <p><a href="{{ route('admin.adminViewCoopDroneInfo') }}">ドローン貸与申請一覧</a></p>
+            <p><a href="{{ route('admin.adminViewUserList') }}">利用者情報管理</a></p>
+            <p><a href="{{ route('admin.adminViewCoopStatisticsInfo') }}">事業者情報分析</a></p>
+            <p><a href="{{ route('admin.adminViewUserStatisticsInfo') }}">利用者情報分析</a></p>
+            <p><a href="{{ route('admin.adminAllocateCoopDeliveryTask') }}">宅配依頼一覧</a></p>
+        </nav>
+        
+        <div class="coopInfo">
+            <p class="information">事業者情報詳細</p>
+            <p class="coopName">{{ $coopName }}</p>
+            <p class="coopId">{{ $coopId }}</p>
+            <table>
+                <tr>
+                    <th>メールアドレス</th>
+                    <th>{{ $data['email'] }}</th>
+                </tr>
+                <tr>
+                    <th>事業者代表名</th>
+                    <th>{{ $data['name'] }}</th>
+                </tr>
+                <tr>
+                    <th>事業者代表名カナ</th>
+                    <th>{{ $data['kanaName'] }}</th>
+                </tr>
+                <tr>
+                    <th>免許情報</th>
+                    <th>{{ $data['licence'] }}</th>
+                </tr>
+                <tr>
+                    <th>口座情報</th>
+                    <th>{{ $data['account'] }}</th>
+                </tr>
+                <tr>
+                    <th>従業員人数</th>
+                    <th>{{ $data['worker'] }}</th>
+                </tr>
+                <tr>
+                    <th>電話番号</th>
+                    <th>{{ $data['phone'] }}</th>
+                </tr>
+                <tr>
+                    <th>申請状態</th>
+                    <th>{{ $data['status'] }}</th>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
