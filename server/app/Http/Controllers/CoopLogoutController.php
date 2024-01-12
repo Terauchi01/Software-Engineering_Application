47288ddbac70270b42df5a3b17b7ba01ed42ae0a
“Terauchi01” <shun1572jp@gmail.com>
2024-01-12 19:55:51 +0900

    return viewのadminとcoopの修正

--- server/app/Http/Controllers/CoopLogoutController.php
+++ server/app/Http/Controllers/CoopLogoutController.php
@@ -9,7 +9,7 @@ class CoopLogoutController extends Controller
     // ログアウト(事業者)
     public function coopLogout (){
         //viewの返すところは適当で良い
-        return view('coop.coopLogout');
+        return view('coop.CoopLogout');
     }
 
     public function coopLogoutFunction (Request $request) {
@@ -17,6 +17,6 @@ class CoopLogoutController extends Controller
             Auth::guard('coops')->logout();
             return redirect()->route('coop.coopLogin');
         }
-        return redirect()->route('coop.coopCheckUserDeliveryRequestListViewExecute');
+        return redirect()->route('coop.CoopCheckUserDeliveryRequestListViewExecute');
     }
 }
