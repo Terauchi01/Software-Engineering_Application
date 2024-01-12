c77188c992614c3bfbec8b9c8d3fd191f8d3fe03
1250333semi <250333k@ugs.kochi-tech.ac.jp>
2024-01-12 19:55:05 +0900

    事業者情報管理画面のバグ修正

--- server/resources/views/admin/AdminViewCoopList.blade.php
+++ server/resources/views/admin/AdminViewCoopList.blade.php
@@ -52,88 +52,17 @@
                 <p><a href="{{ route('admin.adminAllocateCoopDeliveryTask') }}">宅配依頼一覧</a></p>
             </div>
             
-            <button type="submit" name="add" id="filterButton" class="custom-button">絞り込み</button>
-            <button type="submit" name="add" id="resetButton" class="custom-button">リセット</button>
-
-            <script>
-             document.getElementById('filterButton').addEventListener('click', function() {
-                 alert('絞り込みボタンがクリックされました。');
-             });
-
-             document.getElementById('resetButton').addEventListener('click', function() {
-                 alert('リセットボタンがクリックされました。');
-             });
-            </script>
-
-            
-            <p>
-                <input type="checkbox" id="masterCheckbox" name="feature_enabled">
-                <label for="masterCheckbox">Select all</label>
-            </p>            
-            
-            <table class ="coop">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>事業者番号</th>
-                        <th>事業者名</th>
-                        <th>事業者代表名</th>
-                        <th>所在地</th>
-                        <th>先月の支払い状況</th>
-                        <th>編集</th>
-                        <th>削除</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($id as $index => $coopInfo)
-                        <tr>
-                            <td>
-                                <input type="checkbox" class="itemCheckbox" id="checkbox{{$index}}" name="selectedCoops[]" value="{{ $coopInfo }}">
-                            </td>
-                            <td>{{ $id[$index] }}</td>
-                            <td>{{ $coop_name[$index] }}</td>
-                            <td>{{ $representative_name[$index] }}</td>
-                            <td>{{ $coop_locations_list_id[$index] }}</td>
-                            <td>{{ $pay_status[$index] }}</td>
-                            <td><button type="button" onclick="editCoop({{ $id[$index] }})">編集</button></td>
-                            <td><button type="button" onclick="confirmDelete({{ $id[$index] }})">削除</button></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-
-                <script>
-                 document.getElementById('masterCheckbox').addEventListener('change', function() {
-                     var masterCheckbox = this;
-                     var itemCheckboxes = document.querySelectorAll('.itemCheckbox');
-
-                     itemCheckboxes.forEach(function(itemCheckbox) {
-                         itemCheckbox.checked = masterCheckbox.checked;
-                     });
-                 });
-
-                 // 各行のチェックボックスに対するイベントリスナーも追加する場合
-                 document.querySelectorAll('.itemCheckbox').forEach(function(itemCheckbox) {
-                     itemCheckbox.addEventListener('change', function() {
-                         var allChecked = true;
-                         document.querySelectorAll('.itemCheckbox').forEach(function(checkbox) {
-                             if (!checkbox.checked) {
-                                 allChecked = false;
-                             }
-                         }
-                        </script>
+            <div class = "main">
+                <div class ="flex-main">
+                    <div class = "coopList"> <!--#408A7E-->
+                        <p><h2><font color ="#408A7E"><u> 事業者情報管理 </u></font></h2></p>
                     </div>
-                </div>
-                <div class = "main">
-                    <div class ="flex-main">
-                        <div class = "coopList"> <!--#408A7E-->
-                            <p><h2><font color ="#408A7E"><u> 事業者情報管理 </u></font></h2></p>
-                        </div>
-                        
-                        
-
-                        <button id="filterButton" class="custom-button">絞り込み</button>
-                        <!-- <button type="submit" name="add" id="resetButton" class="custom-button">リセット</button> -->
-                        <!-- モーダルダイアログ -->
+                    
+                    
+                    
+                    <button id="filterButton" class="custom-button">絞り込み</button>
+                    <!-- <button type="submit" name="add" id="resetButton" class="custom-button">リセット</button> -->
+                    <!-- モーダルダイアログ -->
                         <div id="modal">
                             <div id="modal-content">
                                 <h3>都道府県を選択してください</h3>
@@ -191,7 +120,7 @@
                                 <button id="applyFilterButton">絞り込み</button>
                             </div>
                         </div>
-
+                        
                         <script>
                          // 絞り込みボタンがクリックされたらモーダルを表示
                          document.getElementById('filterButton').addEventListener('click', function() {
