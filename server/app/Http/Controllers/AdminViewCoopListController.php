436c243b25a7814a01d4be29d108bc4d514d99a4
1250333semi <250333k@ugs.kochi-tech.ac.jp>
2024-01-15 13:46:41 +0900

    コントローラー更新

--- server/app/Http/Controllers/AdminViewCoopListController.php
+++ server/app/Http/Controllers/AdminViewCoopListController.php
@@ -45,45 +45,19 @@ class AdminViewCoopListController extends Controller
                 'coop_name' => $item->coop_name,
                 'representative_name' => $item->representative_last_name." ".$item->representative_first_name,
                 'coop_locations' => $Prefecture_list[$item->prefecture_id].$Cities_list[$item->city_id],
-                'pay_status' => $A[$item->pay_status],
-                //使いそうだから一応コメントアウト
-                // 'child_status' => $item->child_status,
-                // 'pair_id' => $item->pair_id,
-                // 'postal_code' => $item->postal_code,
-                // 'prefecture_id' => $item->prefecture_id,
-                // 'city_id' => $item->city_id,
-                // 'town' => $item->town,
-                // 'block' => $item->block,
+                'pay_status' => $A[$item->pay_status],               
             ];
-        }
-        // ksort($mergedData);
-        // //データ参照用、後で消してs
-        // dump($mergedData);
-        // dump($mergedData[0]);
-        // dump($mergedData[0]['coop_locations']);
-        //viewの返すところは適当で良い
-        // $id = ['1', '2', '3'];
-        // $coop_name = ['土佐山田配送', '高知配送', '南国配送'];
-        // $representative_name = ['山田太郎', '高知花子', '南国次郎'];
-        // $coop_locations_list_id = ['土佐山田', '高知', '南国'];
-        // $pay_status = ['済', '済', '未'];
-        // $land_or_air = ['1', '0', '1'];
+        }   
         return view('admin.AdminViewCoopList', compact('mergedData'));
     }
 
     public function delete(Request $request, $id)
     {
-        // $coopId を使用して削除などの処理を行う
-        // CoopUser::destroy($id);
-        // レスポンスを返す（任意のメッセージを含める）
         $B = CoopUser::class;
         $currentDateTime = Carbon::now();
         $B::where('id',$id)->update(['deletion_date' => $currentDateTime]);
         return redirect()->route('admin.adminViewCoopList');
     }
 }
-// id
-// coop_name
-// representative_name
-// coop_locations_list_id
-// land_or_air
+
+
