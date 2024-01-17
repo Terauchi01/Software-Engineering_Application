b0de3760015ad66ebb093218c290e3d001948028
“Terauchi01” <shun1572jp@gmail.com>
2024-01-17 15:06:35 +0900

    ユーザ退会機能

--- server/app/Http/Controllers/UserWithdrawController.php
+++ server/app/Http/Controllers/UserWithdrawController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\DeliveryRequest;
+use App\Models\DeliveryLocationImage;
+use Carbon\Carbon;
 
 class UserWithdrawController extends Controller
 {
     public function userWithdraw (){
         //viewの返すところは適当で良い
         return view('user.UserWithdraw');
+    }
+    public function withdraw (){
+        $A = 'users';
+        $userId = Auth::guard($A)->id();
+        $exists = DeliveryRequest::where(function ($query) use ($userId) {
+            $query->where('delivery_destination_id', $userId)
+                  ->orWhere('user_id', $userId);
+        })->where('delivery_status', '!=', 4)->where('unpaid_charge','=','0')->exists();
+        // $exists = false;
+        if($exists){
+            return redirect()->back()->with('error', '配達中の荷物が存在します');
+        }
+        else{
+            $user = User::where('id',$userId)->get();
+            User::where('id',$userId)->update(
+                [
+                    'deletion_date' => now(),
+                ]
+            );
+            DeliveryLocationImage::where('user_id',$userId)->update(
+                [
+                    'deletion_date' => now(),
+                ]
+            );
+            Auth::guard('users')->logout();
+        }
+        return redirect()->route('user.userLogin');
     }
 }
--- server/resources/views/user/UserWithdraw.blade.php
+++ server/resources/views/user/UserWithdraw.blade.php
@@ -1,1 +1,14 @@
-userWithdraw test
+@csrf
+<p>本当に退会しますか？</p>
+
+<!-- Yesボタン -->
+<a href="{{ route('user.withdraw') }}">Yes</a>
+
+<!-- Noボタン -->
+<a href="{{ route('user.userDeliveryRequestList') }}">No</a>
+
+@if(session('error'))
+<div class="alert alert-danger">
+    {{ session('error') }}
+</div>
+@endif
