0e6295c03379e0c652001dd1a24541f31c43dc66
Yasushi-Shiozawa <250329v@ugs.kochi-tech.ac.jp>
2024-01-21 23:38:45 +0900

    誤字修正等

--- server/resources/views/user/UserDeliveryRequest.blade.php
+++ server/resources/views/user/UserDeliveryRequest.blade.php
@@ -14,73 +14,6 @@
 @endsection
 
 @section('script')
-
-@endsection
-
-@php
-$currentPage = 'userDeliveryRequest'
-@endphp
-
-@section('content')
-    <div class="contents">
-        <form method="POST" action="{{ route('user.deliveryRequest') }}">
-            @csrf
-            <div class="inputs">
-                <p><h2><font color ="#408A7E"><u> 宅配依頼 </u></font></h2></p>
-                <h2>配送相手の個人情報(相手もこのシステムにユーザ登録している必要があります)</h2>
-                <div class="name">
-                    <label for="user_name">お名前</label>
-                    <input type="text" name="user_last_name" placeholder="性" required>
-                    <input type="text" name="user_first_name" placeholder="名" required>
-                </div>
-
-                <div class="name">
-                    <label for="user_name_kana">お名前(カナ)</label>
-                    <input type="text" name="user_last_name_kana" placeholder="セイ" required>
-                    <input type="text" name="user_first_name_kana" placeholder="メイ" required>
-                </div>
-
-                <!-- 郵便番号 -->
-                <div>
-                    <label for="postal_code">郵便番号</label>
-                    <input type="text" name="postal_code" required>
-                </div>
-
-                <!-- 都道府県id -->
-                <div>
-                    <label for="prefecture_id">都道府県</label>
-                    <select id="prefecture" name="prefecture_id" required>
-                        @foreach ($Prefecture as $id => $name)
-                        <option value="{{ $id }}">{{ $name }}</option>
-                        @endforeach
-                    </select>
-                    <!-- 市区町村のセレクトボックス -->
-                    <label for="city_id">市区町村</label>
-                    <select id="city" name="city_id" required>
-                    </select>
-                </div>
-
-                <!-- それ以降 -->
-                <div>
-                    <label for="town">市区町村以降の住所</label>
-                    <input type="text" name="town" required>
-                    <label for="block">建物名</label>
-                    <input type="text" name="block" required>
-                </div>
-
-                <!-- 宅配物 -->
-                <label for="item">宅配物</label>
-                <input type="text" name="item" required><br>
-
-                <!-- 配達要望日時 -->
-                <label for="request_date">配達要望日時</label>
-                <input type="datetime-local" name="request_date" required>
-            </div>
-            <div class="regist"><button type="submit">送信</button></div>
-        </form>
-    </div>
-@endsection
-
 @if ($errors->any())
 <div>
     <strong>入力エラーがあります。</strong>
@@ -97,7 +30,6 @@ $currentPage = 'userDeliveryRequest'
     <p>{{ session('success') }}</p>
 </div>
 @endif
-
 <script>
     const citiesData = @json($Cities); // コントローラーから渡された市区町村データ
     function updateCities() {
@@ -127,3 +59,69 @@ $currentPage = 'userDeliveryRequest'
     // 初期表示時にも実行
     updateCities();
 </script>
+@endsection
+
+@php
+$currentPage = 'userDeliveryRequest'
+@endphp
+
+@section('content')
+    
+        <form method="POST" action="{{ route('user.deliveryRequest') }}" style="padding-left: 110pt">
+            @csrf
+            <div class="inputs">
+                <p><h2><font color ="#408A7E"><u> 宅配依頼 </u></font></h2></p>
+                <h2>配送相手の個人情報(相手もこのシステムにユーザ登録している必要があります)</h2>
+                <div class="name">
+                    <label for="user_name">お名前</label>
+                    <input type="text" name="user_last_name" placeholder="姓" required style="width: 20%">
+                    <input type="text" name="user_first_name" placeholder="名" required style="width: 20%">
+                </div>
+
+                <div class="name">
+                    <label for="user_name_kana">お名前(カナ)</label>
+                    <input type="text" name="user_last_name_kana" placeholder="セイ" required style="width: 20%">
+                    <input type="text" name="user_first_name_kana" placeholder="メイ" required style="width: 20%">
+                </div>
+
+                <!-- 郵便番号 -->
+                <div>
+                    <label for="postal_code">郵便番号</label>
+                    <input type="text" name="postal_code" required>
+                </div>
+
+                <!-- 都道府県id -->
+                <div>
+                    <label for="prefecture_id">都道府県</label>
+                    <select id="prefecture" name="prefecture_id" required>
+                        @foreach ($Prefecture as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                        @endforeach
+                    </select>
+                    <!-- 市区町村のセレクトボックス -->
+                    <label for="city_id">市区町村</label>
+                    <select id="city" name="city_id" required>
+                    </select>
+                </div>
+
+                <!-- それ以降 -->
+                <div>
+                    <label for="town">市区町村以降の住所</label>
+                    <input type="text" name="town" required>
+                </div>
+                <div>
+                    <label for="block">建物名</label>
+                    <input type="text" name="block" required>
+                </div>
+
+                <!-- 宅配物 -->
+                <label for="item">宅配物</label>
+                <input type="text" name="item" required><br>
+
+                <!-- 配達要望日時 -->
+                <label for="request_date">配達要望日時</label>
+                <input type="datetime-local" name="request_date" required>
+            </div>
+            <div class="regist"><button type="submit">送信</button></div>
+        </form>
+@endsection
