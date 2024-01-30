8aac2c01ce23a5e32780ef444f9d75f022cd68d0
S_Matsumoto <250371g@ugs.kochi-tech.ac.jp>
2024-01-30 14:51:48 +0900

    create allocate

--- server/resources/views/admin/AdminAllocateCoopDeliveryTask.blade.php
+++ server/resources/views/admin/AdminAllocateCoopDeliveryTask.blade.php
@@ -21,40 +21,76 @@ $currentPage = 'adminAllocateCoopDeliveryTask'
 @endphp
 
 @section('content')
-<div class = "main">
-    <div class ="flex-main">            
+<div class="main">
+    <div class="flex-main">            
         <p><h2><font color ="#408A7E"><u> 宅配依頼割り振り一覧 </u></font></h2></p>
-              
-        <br></br>
         
-        <table class ="coop">
-            <thead>
-                <tre>                   
-                    <th>依頼番号</th>
-                    <th>送り主情報</th>
-                    <th>受け取り主情報</th>
-                    <th>担当者事業者名</th>                 
-                    <th>割り振り</th>           
-                    <th>削除</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($mergedData as $index => $deliveryInfo)
-                    <tr>
-                        <td>{{ $deliveryInfo['id'] }}</td>
-                        <td>{{ $deliveryInfo['user_id'] }}</td>
-                        <td>{{ $deliveryInfo['delivery_destination_id'] }}</td>
-                        <td>{{ $deliveryInfo['delivery_company_id'] }}</td>                        
-                        <td><button type="button">
-                            <a href="{{ route('admin.adminAllocateCoopDeliveryTaskApproval', ['id' => $deliveryInfo['id']])  }}">
-                                <img src="{{ asset('image/img_approval.png') }}" alt="承認" width="20" height="20"</a></button></td>
-                        <td><button type="button">
-                            <a href="{{ route('admin.adminAllocateCoopDeliveryTaskDelete', ['id' => $deliveryInfo['id']]) }}">
-                               <img src="{{ asset('image/img_delete.png') }}" alt="削除" width="20" height="20"></a></button></td>
+        <br></br>
+
+        <form action="{{ route('admin.adminAllocateCoopDeliveryTaskApproval') }}" method="post">
+            @csrf
+            
+            <table class="coop">
+                <thead>
+                    <tr>                   
+                        <th>依頼番号</th>
+                        <th>送り主情報</th>
+                        <th>受け取り主情報</th>
+                        <th>集荷担当者事業者名</th>
+                        <th>中間担当者事業者名</th>
+                        <th>配達担当者事業者名</th>
+                        <th>割り振り</th>
+                        <th>削除</th>
                     </tr>
+                </thead>
+                <tbody>
+                    @foreach ($mergedData as $index => $deliveryInfo)
+                        <tr>
+                            <td>{{ $deliveryInfo['id'] }}</td>
+                            <td>{{ $deliveryInfo['user_id'] }}</td>
+                            <td>{{ $deliveryInfo['delivery_destination_id'] }}</td>
+                            <td>
+                                <select name="c_coop_id">
+                                    <option value="">-- 選択してください --</option>
+                                    @foreach($coops as $coop)
+                                    <option value="{{ $coop->id }}">{{ $coop->coop_name }}</option>
                     @endforeach
-            </tbody>        
-        </table>
+                                </select>
+                            </td>
+                            <td>
+                                <select name="i_coop_id">
+                                    <option value="">-- 選択してください --</option>
+                                    @foreach($coops as $coop)
+                                    <option value="{{ $coop->id }}">{{ $coop->coop_name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>          
+                            <td>
+                                <select name="d_coop_id">
+                                    <option value="">-- 選択してください --</option>
+                                    @foreach($coops as $coop)
+                                    <option value="{{ $coop->id }}">{{ $coop->coop_name }}</option>
+                                    @endforeach
+                                </select>
+                            </td>          
+
+                            <td>
+                                <button type="submit" name="id" value="{{ $deliveryInfo['id'] }}">
+                                    <img src="{{ asset('image/img_approval.png') }}" alt="承認" width="20" height="20">
+                                </button>
+                            </td>
+                            <td>
+                                <button type="button">
+                                    <a href="{{ route('admin.adminAllocateCoopDeliveryTaskDelete', ['id' => $deliveryInfo['id']]) }}">
+                                        <img src="{{ asset('image/img_delete.png') }}" alt="削除" width="20" height="20">
+                                    </a>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>        
+            </table>
+        </form>
     </div>
 </div>
 @endsection
